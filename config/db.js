8ab7ecfd9e1f9d50a8954d56f29ea5e9bc2fda0d
vinyasbj <vinyas555@gmail.com>
2020-03-31 20:56:55 +0530

    mongo db added

--- config/db.js
+++ config/db.js
@@ -1,12 +1,18 @@
 // blog_database
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise
+const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://heroku_f4cwmxmm:r7fhkli2hi34pchn6qur3jjhlq@ds119765.mlab.com:19765/heroku_f4cwmxmm';
+// mongoose.connect('mongodb://localhost:27017/blog_database', { useNewUrlParser: true }).then((res) => {
+//     console.log('connected to blog_database'); 
+// }).catch((err) => {
+//     console.log(err); 
+// });
 
-mongoose.connect('mongodb://localhost:27017/blog_database', { useNewUrlParser: true }).then((res) => {
-    console.log('connected to blog_database'); 
+mongoose.connect(CONNECTION_URI, { useNewUrlParser: true }).then((res) => {
+    console.log('connected to db'); 
 }).catch((err) => {
     console.log(err); 
-});
+}); 
 
 module.exports = {
     mongoose
